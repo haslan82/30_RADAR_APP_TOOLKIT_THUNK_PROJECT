1be965ab9bc57a8e946bfbdf5f17aa40cbc20633
haslan82 <haslan82@hotmail.com>
2024-06-24 20:16:15 +0300

    ekledim

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { dOptions } from "../constants";
 import { useDispatch } from "react-redux";
 import { setPath } from "../redux/slices/flightSlice";
-
+import formatDate from "../utils/formatDate";
 const Modal = ({ detailId }) => {
   //! console.log(detailId)
 
@@ -24,19 +24,86 @@ const Modal = ({ detailId }) => {
         dispatch(setPath(res.data.trail));
       });
   }, [detailId]);
+console.log(d)
+return (
+    <div className="detail-outer">
+      <div className="detail-inner">
+        <div className="close-wrapper">
+          <button onClick={close}>X</button>
+        </div>
 
-  return(
-    
-   <div className="detail-outer">
-<div className="detail-inner">
-    <div className="close-wrapper">
-        <button>X</button>
+        {!d ? (
+          <div className="loader-wrapper">
+            <div className="loader">
+              <span></span>
+            </div>
+          </div>
+        ) : (
+          <>
+            <h2>{d.aircraft.model.text}</h2>
+            <h2>{d.aircraft.model.code}</h2>
+
+            <p>
+              <span>Kuyruk Kodu</span>
+              <span>{d.aircraft.registration}</span>
+            </p>
+
+            {d.aircraft.images ? (
+              <img
+                src={
+                  d.aircraft.images?.large
+                    ? d.aircraft.images.large[0].src
+                    : d.aircraft.images.thumbnails[0].src
+                }
+                alt=""
+              />
+            ) : (
+              <p>Fotoğraf Bulunamadı</p>
+            )}
+
+            <p>
+              <span>Şirket:</span>
+              <span>{d.airline?.short}</span>
+            </p>
+
+            <p>
+              <span>Kalkış:</span>
+              <a href={d.airport?.origin?.website} target="_blank">
+                {d.airport?.origin?.name}
+              </a>
+            </p>
+            <p>
+              <span>İniş:</span>
+              <a href={d.airport?.destination?.website} target="_blank">
+                {d.airport?.destination?.name}
+              </a>
+            </p>
+
+            <p>
+              <span>Kalkış Zamanı</span>
+              <span>
+                {d.time?.scheduled?.departure > 0
+                  ? formatDate(d.time?.scheduled?.departure)
+                  : "Bilinmiyor"}
+              </span>
+            </p>
+            <p>
+              <span>İniş Zamanı</span>
+              <span>
+                {d.time?.scheduled?.departure > 0
+                  ? formatDate(d.time?.scheduled?.arrival)
+                  : "Bilinmiyor"}
+              </span>
+            </p>
+
+            <p className={d.status?.icon}>
+              <span>{d.status.text}</span>
+            </p>
+          </>
+        )}
+      </div>
     </div>
-</div>
-
-  </div>
-  
-)
+  );
 };
 
 export default Modal;
